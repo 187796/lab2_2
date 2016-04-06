d3b245cd986ba1f916b6faad60ac71ee355cb45f
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-04-06 18:18:04 +0200

    Dodanie testow dla metody add z klasy Money

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -34,4 +34,31 @@ public class MoneyTest {
 		assertThat(result.toString(), is("100,00 " + Money.DEFAULT_CURRENCY));
 	}
 
+	@Test
+	public void testAddWithPositiveNumber() {
+		Money firstMoney = new Money(100);
+		Money secondMoney = new Money(50);
+
+		Money result = firstMoney.add(secondMoney);
+		assertThat(result.toString(), is("150,00 " + Money.DEFAULT_CURRENCY));
+	}
+
+	@Test
+	public void testAddWithNegativeNumber() {
+		Money firstMoney = new Money(100);
+		Money secondMoney = new Money(-99);
+
+		Money result = firstMoney.add(secondMoney);
+		assertThat(result.toString(), is("1,00 " + Money.DEFAULT_CURRENCY));
+	}
+
+	@Test
+	public void testAddWithZeroNumber() {
+		Money firstMoney = new Money(100);
+		Money secondMoney = new Money(0);
+
+		Money result = firstMoney.add(secondMoney);
+		assertThat(result.toString(), is("100,00 " + Money.DEFAULT_CURRENCY));
+	}
+
 }
